56b4993608d01956e31de07f048516173a197f56
Mohamed Waseem <mohwaseem13@gmail.com>
2023-02-28 18:56:04 +0530

    Initial commit

---
+++ app/src/main/java/com/wsm/gettext/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.wsm.gettext
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.wsm.gettext.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val PERMISSION_CODE = 100
+    }
+
+    private val REQUEST_IMAGE_GET = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imagePicker =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                uri?.let {
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, it)
+                    binding.ivImage.setImageBitmap(bitmap)
+                    recognizeText(bitmap)
+                }
+            }
+
+        binding.btnAddImage.setOnClickListener {
+//            imagePicker.launch("image/*")
+
+            val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Add Photo!")
+            builder.setItems(options) { dialog, item ->
+                when {
+                    options[item] == "Take Photo" -> {
+                        if (checkPermission()) {
+                            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                            }
+                        } else {
+                            requestPermission()
+                        }
+                    }
+                    options[item] == "Choose from Gallery" -> {
+                        if (checkPermission()) {
+                            val intent = Intent(Intent.ACTION_GET_CONTENT)
+                            intent.type = "image/*"
+                            if (intent.resolveActivity(packageManager) != null) {
+                                startActivityForResult(intent, REQUEST_IMAGE_GET)
+                            }
+                        } else {
+                            requestPermission()
+                        }
+                    }
+                    options[item] == "Cancel" -> {
+                        dialog.dismiss()
+                    }
+                }
+            }
+            builder.show()
+
+        }
+
+        if (!checkPermission()) {
+            requestPermission()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_GET -> {
+                    val selectedImageUri = data?.data
+                    // Handle selected image URI
+                    selectedImageUri?.let {
+                        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, it)
+                        binding.ivImage.setImageBitmap(bitmap)
+                        recognizeText(bitmap)
+                    }
+                }
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    // Handle captured image bitmap
+                    binding.ivImage.setImageBitmap(imageBitmap)
+                    recognizeText(imageBitmap)
+                }
+            }
+        }
+    }
+
+
+    private fun recognizeText(bitmap: Bitmap) {
+        val recognizer = TextRecognition.getClient()
+        val image = InputImage.fromBitmap(bitmap, 0)
+        recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                val resultText = visionText.text
+                binding.tvContent.text = resultText
+            }
+            .addOnFailureListener { e ->
+                binding.tvContent.text = "Failed to recognize text: ${e.localizedMessage}"
+            }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA),
+            PERMISSION_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+}
